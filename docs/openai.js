48a875e59e275aef63e78595bc1617f0c05d4439
master <alberto.mastroianni@istitutoagnelli.it>
2024-05-01 12:51:27 +0200

    aggiunta favicon + qualche informazione da modificare su scritta: biglietti

--- docs/openai.js
+++ docs/openai.js
@@ -1,6 +1,11 @@
 const OpenAI = require("openai");
 require("dotenv").config();
 
+
+const axios = require('axios');
+
+
+
 const openai = new OpenAI({
   apiKey: process.env.sk-proj-GkqoCy0IHLTHnkA2iOqKT3BlbkFJektO7FQh34pMZsse1623,
 });
@@ -11,7 +16,7 @@ async function getAIResponse(prompt) {
       if (prompt.toLowerCase().includes("assistenza")) {
         // Simulo un caricamento con un ritardo di 2 secondi
         await simulateLoading(2000);
-        return "Puoi contattarci all'indirizzo e-mail: assistenza@chatgtt.it. Proveremo a soddisfare ogni tua richiesta!";
+        return "Puoi contattarci all'indirizzo e-mail: assistenza@chatgtt.it Proveremo a soddisfare ogni tua richiesta!";
       }
       if (prompt.toLowerCase().includes("info")) {
         // Simulo un caricamento con un ritardo di 2 secondi
@@ -19,6 +24,21 @@ async function getAIResponse(prompt) {
         // Restituisci una risposta di informazioni
         return "L'idea è nata da un progetto di 5^ superiore, realizzato da Alberto Mastroianni e Matteo Licciardino, appartenti all'Istituto tecnico E. Agnelli Indirizzo informatico. In particolare Alberto ha sempre avuto la passione per i mezzi locali, focalizzandosi sull'azienda di trasporti pubblici locali GTT (Gruppo Torinese Trasporti) dove ha mostrato un grande interesse; così ha coinvolto il suo compagno Matteo e insieme hanno tirato fuori un'idea innovativa, che consente di aiutare i torinesi e i turisti a poter usare i servizi pubblici in modo comodo e veloce. Benvenuti su ChatGTT, il vostro aiutante virtuale. ";
       }
+      if (prompt.toLowerCase().includes("biglietti")) {
+        await simulateLoading(2000);
+        const messages = [
+            "Ecco a te la lista dei biglietti urbani:",
+            "Biglietto City (2,00€)",
+            "MultiCity (11,80€)",
+            "Daily (4,50€)",
+            "MultiDaily 7 (21,00€)",
+            "Biglietto Speciale 'Tour' valido per 48 o 72 ore (9,50€ / 12,50€)",
+            "Biglietto Integrato A e B (A= 3,70€ / B= 4,20€)",
+            "Descrizioni:",
+            "Biglietto City: Vale per 100 minuti dal momento della convalida compresa una corsa in Metropolitana. E' utilizzabile solo da una persona e deve essere validato ogni volta che si cambia mezzo. Il biglietto City caricato su smart card BIP ha validità 730 giorni dal momento dell'acquisto e di 365 giorni se acquistato con app To Move."
+        ];
+        return messages;
+    }
       
       // Altrimenti, utilizzo OpenAI per ottenere una risposta
       const response = await openai.chatCompletion.create({
@@ -54,3 +74,5 @@ async function getAIResponse(prompt) {
     });
   }
     
+
+  
